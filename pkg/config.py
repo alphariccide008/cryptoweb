2995ba12df75d0cdde1a33005721402cc182a624
alpha <godspowerlawrence008@gmail.com>
2024-10-30 08:55:43 -0400

    new update

--- pkg/config.py
+++ pkg/config.py
@@ -9,4 +9,6 @@ host = getenv('CRYPTOWEB_HOST')
 SECRET_KEY="R5T6Y7UHJIKOLO987EROELFKGWASDDJNRFTGHVN"
 ADMIN_EMAIL="godspowerlawrence008@gmail.com"
 USER_PROFILE_PATH="pkg/static/profiles/"
-SQLALCHEMY_DATABASE_URI=f'mysql+mysqlconnector://{user}:{password}@{host}/{database}'
+# SQLALCHEMY_DATABASE_URI=f'mysql+mysqlconnector://{user}:{password}@{host}/{database}'
+SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://lawrence:1234@127.0.0.1:8889/crypto_db"
+
